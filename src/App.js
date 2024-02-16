b0dacea4d4fcaecb78f577e7f62c4ff33ea3eb19
Sufiyan Ansari <sufiyanansari0617@gmail.com>
2024-02-16 14:45:18 +0530

    login api working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import "./App.css";
+// import "./App.css";
+// import Login from "./components/Login";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Login />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// src/App.js
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,115 +1,33 @@
-// import React, { useState } from "react";
-// import Logo from "../assets/logo.jpeg";
-// import { Box, Button, TextField } from "@mui/material";
-
-// function Login() {
-//   const [showPassword, setShowPassword] = useState(false);
-
-//   const showPass = () => {
-//     setShowPassword(!showPassword);
-//   };
-
-//   return (
-//     <Box
-//       sx={{
-//         width: "95vw",
-//         height: "93vh",
-//         display: "flex",
-//         justifyContent: "center",
-//         alignItems: "center",
-//       }}
-//     >
-//       <Box
-//         sx={{
-//           width: { xs: "80%", md: "60%" },
-//           height: { xs: "80vh", md: "40vh" },
-//           display: "flex",
-//           flexDirection: { xs: "column", md: "row" },
-//           borderRadius: "10px",
-//           border: "3px solid #51e2f5",
-//         }}
-//       >
-//         <Box
-//           sx={{
-//             width: { xs: "100%", md: "40%" },
-//             height: { xs: "50%", md: "100%" },
-//             display: "flex",
-//             justifyContent: "center",
-//             alignItems: "center",
-//           }}
-//         >
-//           <img src={Logo} alt="Logo" />
-//         </Box>
-//         <Box
-//           sx={{
-//             width: { xs: "100%", md: "60%" },
-//             height: { xs: "50%", md: "100%" },
-//             display: "flex",
-//             flexDirection: "column",
-//             justifyContent: "center",
-//             alignItems: "center",
-//             gap: "10px",
-//           }}
-//         >
-//           <TextField size="small" label="Email" />
-//           <TextField
-//             size="small"
-//             type={showPassword ? "text" : "password"}
-//             label="Password"
-//           />
-//           <p
-//             onClick={showPass}
-//             style={{
-//               cursor: "pointer",
-//               borderBottom: "2px solid #51e2f5",
-//               color: "#51e2f5",
-//             }}
-//           >
-//             {showPassword ? "Hide Password" : "Show Password"}
-//           </p>
-//           <Button variant="contained">Login</Button>
-//         </Box>
-//       </Box>
-//     </Box>
-//   );
-// }
-
-// export default Login;
-
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Logo from "../assets/logo.jpeg";
 import { Box, Button, TextField } from "@mui/material";
 import axios from "axios";
 
 function Login() {
   const [showPassword, setShowPassword] = useState(false);
-  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const showPass = () => {
     setShowPassword(!showPassword);
   };
 
+
+
   const handleLogin = async () => {
     try {
       const response = await axios.post(
-        "https://reacttestprojectapi.azurewebsites.net/api/UserManagement/AuthenticateUser",
-        {
-          UserName: email,
-          Password: password,
-        }
+        `https://reacttestprojectapi.azurewebsites.net/api/UserManagement/AuthenticateUser?UserName=${userName}&Password=${password}`
       );
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      // Assuming the token is present in the response
-      const authToken = response.data.token;
+      const authToken = response.data.authToken;
+      sessionStorage.setItem('authToken', authToken);
       console.log("Authentication successful. Token:", authToken);
+      navigate("/dashboard");
 
-      // You can save the token in state or a global context for further use.
     } catch (error) {
       setError(`Authentication failed. Error: ${error.message}`);
       console.error("Authentication error:", error);
@@ -161,9 +79,9 @@ function Login() {
         >
           <TextField
             size="small"
-            label="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            label="User Name"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
           />
           <TextField
             size="small"
